1916fc4f8fae7b7011158503ad517bb1a6be0d0a
gihani571 <gihanimadhubashini75@gmail.com>
2025-02-24 14:29:52 +0530

    create login and dashboard page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./page/login/login.component";
+import { DashboardComponent } from "./page/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
